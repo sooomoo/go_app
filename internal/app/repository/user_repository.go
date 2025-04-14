fb63c6231d44a4910bcb4dd1c6be1aeed80f1e3a
mur <mur@qq.com>
2025-04-14 15:09:03 +0800

    2

--- internal/app/repository/user_repository.go
+++ internal/app/repository/user_repository.go
@@ -8,8 +8,8 @@ import (
 	"time"
 
 	"github.com/sooomo/niu"
-	"gorm.io/gen/field"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 const (
@@ -45,28 +45,14 @@ func (r *UserRepository) Upsert(ctx context.Context, phone, ip string) (*model.U
 		return nil, err
 	}
 
-	// var ret model.User
-	// err = r.query.User.WithContext(ctx).Clauses(clause.OnConflict{
-	// 	Columns: []clause.Column{{Name: r.query.User.Phone.ColumnName().String()}},
-	// 	DoUpdates: clause.Assignments(map[string]any{
-	// 		r.query.User.UpdatedAt.ColumnName().String(): time.Now().Unix(),
-	// 		r.query.User.IPLatest.ColumnName().String():  ip,
-	// 	}),
-	// }).DO.Returning(&ret).Create(&model.User{
-	// 	ID:        int64(userId),
-	// 	Phone:     phone,
-	// 	Name:      phone[3:6] + "****" + phone[10:],
-	// 	Role:      int32(RoleNormal),
-	// 	Status:    UserStatusNormal,
-	// 	IPInit:    ip,
-	// 	IPLatest:  ip,
-	// 	CreatedAt: time.Now().Unix(),
-	// 	UpdatedAt: time.Now().Unix(),
-	// })
-	ret, err := r.query.User.WithContext(ctx).Assign(field.Attrs(&model.User{
-		IPLatest:  ip,
-		UpdatedAt: time.Now().Unix(),
-	})).Attrs(field.Attrs(&model.User{
+	u := r.query.User
+	err = u.WithContext(ctx).Clauses(clause.OnConflict{
+		Columns: []clause.Column{{Name: u.Phone.ColumnName().String()}},
+		DoUpdates: clause.Assignments(map[string]any{
+			u.UpdatedAt.ColumnName().String(): time.Now().Unix(),
+			u.IPLatest.ColumnName().String():  ip,
+		}),
+	}).Create(&model.User{
 		ID:        int64(userId),
 		Phone:     phone,
 		Name:      phone[3:6] + "****" + phone[10:],
@@ -76,11 +62,12 @@ func (r *UserRepository) Upsert(ctx context.Context, phone, ip string) (*model.U
 		IPLatest:  ip,
 		CreatedAt: time.Now().Unix(),
 		UpdatedAt: time.Now().Unix(),
-	})).Where(r.query.User.Phone.Eq(phone)).FirstOrCreate()
+	})
 	if err != nil {
 		return nil, err
 	}
-	return ret, nil
+
+	return u.WithContext(ctx).Where(u.Phone.Eq(phone)).First()
 }
 
 func (r *UserRepository) GetById(ctx context.Context, userId int64) (*model.User, error) {
